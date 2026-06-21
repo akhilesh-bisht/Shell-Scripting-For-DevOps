2785b1e88b1743646a1e052e9bfda017947c1c35
Akhilesh <akhileshbisht2020@gmail.com>
2026-06-21 21:58:03 +0530

    refactor: update script content to reflect Day 1 practice task

---
+++ day01/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is Day 1 practice script
+# Task: Print your basic details on the screen
+
+echo "Hello Friends"
+echo "My Name: DevOps Learner"
+echo "My Goal: Learn Bash Scripting"
+echo "Today Task: Print messages using echo command"
